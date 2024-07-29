e33fbdc6c4b53df968f234aa6a3852191958832b
Alan McGraw <alanmcgraw@proton.me>
2024-07-29 09:53:42 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Junglebook</h1>
+      </header>
+      <p>This app is a social media platform for pets and all things pet ownership.</p>
+      <main>
+        Place Holder
+      </main>
+
+    </>
+  )
+}
